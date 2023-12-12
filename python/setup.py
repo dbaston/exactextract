681be315878bc450706763fbde86a6b54cd0e986
Daniel Baston <dbaston@gmail.com>
2023-12-12 14:20:38 -0500

    Python: Add setup.py script

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from skbuild import setup
+
+setup(
+    name="exactextract",
+    version="0.2",
+    url="https://github.com/isciences/exactextract",
+    cmake_args=["-DCMAKE_BUILD_TYPE=Release", "-DBUILD_CLI=NO", "-DBUILD_TEST=NO", "-DBUILD_DOC=NO"],
+    cmake_source_dir="..",
+    cmake_install_target="_exactextract",
+    packages=["exactextract"],
+    package_dir={"exactextract":"src/exactextract"}
+)
